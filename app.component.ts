fca6e07840a795bc802d50d99db97244b18b0559
gfortune1 <fgeee1@gmail.com>
2023-11-05 17:49:31 +0000

    final

---
+++ app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'about.page.html',
+  styleUrls: ['app.component.ts']
+})
+export class AppComponent { }
+
